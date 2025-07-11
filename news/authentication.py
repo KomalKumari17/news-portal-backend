cc8d5e4cd5804df3aefc3a1e4d09a5feaf4489af
KomalKumari17 <komalydv2760@gmail.com>
2025-07-12 00:57:50 +0530

    Refactor CookieJWTAuthentication to use configurable cookie name for token retrieval

--- news/authentication.py
+++ news/authentication.py
@@ -1,10 +1,16 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import exceptions
+from django.conf import settings
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        access_token = request.COOKIES.get('access_token')
-        if not access_token:
-            return None
-        validated_token = self.get_validated_token(access_token)
-        return self.get_user(validated_token), validated_token
+        cookie_name = getattr(settings, 'SIMPLE_JWT', {}).get('AUTH_COOKIE', 'access_token')
+        raw_token = request.COOKIES.get(cookie_name)
+        if not raw_token:
+            return None  # No token found in cookies
+        try:
+            validated_token = self.get_validated_token(raw_token)
+            user = self.get_user(validated_token)
+            return (user, validated_token)
+        except exceptions.AuthenticationFailed as e:
+            raise exceptions.AuthenticationFailed('Invalid token in cookie') from e
